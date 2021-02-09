f0537c05cacc923ec20d35e7067185dc4d79bcb3
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-09 15:00:06 +0000

    refactored index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+function sum(fromN, toN){
+  if (fromN === toN){
+    return fromN;
+  } else {
+    return fromN + sum(fromN + 1, toN);
+  }
+};
 
 module.exports = sum;
+
+// sum from 1 to 1 = 1
+// sum from 2 to 1
